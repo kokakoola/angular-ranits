9c01d52abd66f878e5c6c366e2aba311b904fe21
mare hunt <mare.hunt@netgroup.ee>
2015-11-02 17:18:19 +0200

    left sidebar logic

--- app/scripts/controllers/add.js
+++ app/scripts/controllers/add.js
@@ -9,10 +9,10 @@
 */
 
 angular.module('ranitsApp')
-.controller('AddCtrl', function($scope, $timeout, $mdSidenav, $mdUtil, $log, $mdBottomSheet){
-  $scope.toggleSidenav = function(menuId) {
-    $mdSidenav(menuId).toggle();
-  };
+.controller('AddCtrl', function($scope, $timeout, $mdSidenav, $mdUtil, $log){
+  // $scope.toggleSidenav = function(menuId) {
+  //   $mdSidenav(menuId).toggle();
+  // };
 
   $scope.toggleLeft = buildToggler('left');
 
@@ -52,35 +52,35 @@ angular.module('ranitsApp')
 
 
 })
-.controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-  $scope.close = function () {
-    $mdSidenav('left').close()
-    .then(function () {
-      $log.debug("close LEFT is done");
-    });
-
-  };
-})
-.directive('sidenavPushIn', function () {
-  return {
-    restrict: 'A',
-    require: '^mdSidenav',
-    link: function ($scope, element, attr, sidenavCtrl) {
-      var body = angular.element(document.body);
-      body.addClass('md-sidenav-push-in');
-      var cssClass = (element.hasClass('md-sidenav-left') ? 'md-sidenav-left' : 'md-sidenav-right') + '-open';
-      var stateChanged = function (state) {
-        body[state ? 'addClass' : 'removeClass'](cssClass);
-      };
-      // overvwrite default functions and forward current state to custom function
-      angular.forEach(['open', 'close', 'toggle'], function (fn) {
-        var org = sidenavCtrl[fn];
-        sidenavCtrl[fn] = function () {
-          var res = org.apply(sidenavCtrl, arguments);
-          stateChanged(sidenavCtrl.isOpen());
-          return res;
-        };
-      });
-    }
-  };
-});
+// .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+//   $scope.close = function () {
+//     $mdSidenav('left').close()
+//     .then(function () {
+//       $log.debug("close LEFT is done");
+//     });
+//
+//   };
+// })
+// .directive('sidenavPushIn', function () {
+//   return {
+//     restrict: 'A',
+//     require: '^mdSidenav',
+//     link: function ($scope, element, attr, sidenavCtrl) {
+//       var body = angular.element(document.body);
+//       body.addClass('md-sidenav-push-in');
+//       var cssClass = (element.hasClass('md-sidenav-left') ? 'md-sidenav-left' : 'md-sidenav-right') + '-open';
+//       var stateChanged = function (state) {
+//         body[state ? 'addClass' : 'removeClass'](cssClass);
+//       };
+//       // overvwrite default functions and forward current state to custom function
+//       angular.forEach(['open', 'close', 'toggle'], function (fn) {
+//         var org = sidenavCtrl[fn];
+//         sidenavCtrl[fn] = function () {
+//           var res = org.apply(sidenavCtrl, arguments);
+//           stateChanged(sidenavCtrl.isOpen());
+//           return res;
+//         };
+//       });
+//     }
+//   };
+// });

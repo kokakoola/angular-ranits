bdea00c395768ec9eda8d471b24a9ae02c1ff5fe
Aare-Arnold Äkke <aare.arnold.akke@gmail.com>
2015-11-16 16:02:22 +0200

    home view fixes

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -1,19 +1,17 @@
 define(['app'], function(app)
 {
-    app.controller('homeController', ['$scope', "serverCallService", 'translationService',
-    		function($scope, serverCallService, translationService) {
+    app.controller('homeController', ['$scope', "serverCallService", 'translationService', function($scope, serverCallService, translationService) {
         var params = {};
     	serverCallService.makeGet("rest/material/getNewestMaterials?numberOfMaterials=8", params, getNewestMaterialsSuccess, getNewestMaterialsFail);
-    	
-    	function getNewestMaterialsSuccess(data) {
 
+    	function getNewestMaterialsSuccess(data) {
             if (isEmpty(data)) {
                 log('No data returned by session search.');
-                } else {
-                        $scope.materials = data;
-                }
+            } else {
+                $scope.materials = data;
+            }
     	}
-    	
+
     	function getNewestMaterialsFail(data, status) {
             console.log('Session search failed.')
     	}
